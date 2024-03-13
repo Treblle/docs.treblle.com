51d32161d31ba06210dc1442c1cc1a6ed9671c97
Steve McDougall <juststevemcd@gmail.com>
2024-03-13 12:18:23 +0000

    Making the docs better

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,15 @@ export default defineConfig({
   site: 'https://docs.treblle.com',
   integrations: [starlight({
     title: 'Treblle',
-		
+    description: 'Treblle is your End to End API Ops platform, helping engineering and product teams build, ship and understand their REST APis in one single place.',
+    editLink: {
+      baseUrl: 'https://github.com/treblle/docs.treblle.com/edit/main/',
+    },
+    logo: {
+      dark: './src/assets/logo-white.svg',
+      light: './src/assets/logo-black.svg',
+      replacesTitle: true
+    },
     social: {
       github: 'https://github.com/treblle',
       twitter: 'https://twitter.com/treblleapi',
@@ -17,6 +25,7 @@ export default defineConfig({
 		customCss: [
 			'./src/tailwind.css'
 		],
+    lastUpdated: true,
     sidebar: [{
       label: 'Who is Treblle for?',
       link: '/who'
@@ -24,19 +33,15 @@ export default defineConfig({
       label: 'How Treblle works',
       link: '/how'
     }, {
-      label: 'Guides',
+      label: 'Treblle',
       autogenerate: {
-        directory: 'guides'
+        directory: 'treblle'
       }
     }, {
       label: 'Integrations',
+      collapsed: true,
       autogenerate: {
         directory: 'integrations'
-      }
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
       }
     }]
   }), tailwind({
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,19 @@
 import starlightPlugin from '@astrojs/starlight-tailwind';
-import colors from 'tailwindcss/colors';
+import aspectRatio from '@tailwindcss/aspect-ratio';
+
+const accent = { 200: '#9ea2bd', 600: '#0058ff', 900: '#0047bb', 950: '#2d3f58' };
+const gray = { 100: '#fff', 200: '#eeeeee', 300: '#c2c2c2', 400: '#8b8b8b', 500: '#585858', 700: '#383838', 800: '#01041a', 900: '#01041a' };
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			colors: {
-				accent: colors.indigo,
-				gray: colors.zinc,
-			}
+			colors: { accent, gray }
 		},
 	},
 	plugins: [
-		starlightPlugin()
+		starlightPlugin(),
+		aspectRatio,
 	],
 }
