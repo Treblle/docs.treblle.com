299afbcfdef9b1538b8a7811dc5f6baf42dfe471
Steve McDougall <juststevemcd@gmail.com>
2024-04-19 09:42:16 +0100

    Expanding integrations menu by request of Josip

--- astro.config.mjs
+++ astro.config.mjs
@@ -46,7 +46,7 @@ export default defineConfig({
       }
     }, {
       label: 'Integrations',
-      collapsed: true,
+      collapsed: false,
       autogenerate: {
         directory: 'integrations'
       }
